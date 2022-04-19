afacbf3479586cb1d50fd64a9e3ff2c993a4c68a
18758407926 <103395362+18758407926@users.noreply.github.com>
2022-04-19 18:22:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction(){
+var x = document.getElementById("num1").value;
+var y = document.getElementById("num2").value;
+var z= Number(x) + Number(y);
+document.getElementById("num3").innerHTML = z ;
+}
